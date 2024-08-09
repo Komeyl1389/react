fee217814c39660d86b7797860de81caee345528
react <Komeyleskandari1@gmail.com>
2024-08-09 16:02:56 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+const App = () => {
+const k = 'komeil';
+const q = 20;
+const b = 1000 ;
+const names = ['komeil','amir']
+
+  return(
+    <div>
+    <div className='text-5x1'>App</div>
+    {/* <p>hello {k}</p> */}
+    <p>
+      the sum of {q} and {b} is {q+b}
+    </p>
+    <ul>
+      { names.map((name, index) => (
+        <li key={index}>{name}</li>
+      )) }
+    </ul>
+    </div>
+  )
+};
+export default App;
